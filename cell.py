e8c5487402a8295fd83ccb5abc5c509f188a80fd
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2021-05-04 11:59:22 +0200

    Create cell.py

---
+++ cell.py
@@ -0,0 +1,14 @@
+class Cell:
+    def __init__(self, n):
+        self.n = n
+        self.cells_unvisited= [None, None, None, None]
+        self.cells = [None, None, None, None]
+
+    def set_visited(self):
+        for i in range(4):
+            try:
+                self.cells[i].cells_unvisited[i - 2] = None
+                #print(self.cells_unvisited[i].n, self.cells_unvisited[i].cells_unvisited)
+            except Exception:
+                pass
+
